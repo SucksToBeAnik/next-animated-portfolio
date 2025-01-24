98dae76fbabe9503cb5218a63a2097e48b0449f8
Al Jami Islam Anik <anik.islam1494@gmail.com>
2025-01-24 23:05:19 +0600

    fixed bugs

--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -7,8 +7,15 @@ function NavLink({ link }) {
   const pathName = usePathname();
 
   return (
-    <Link href={link.url} className={`rounded px-2 py-1 ${pathName === link.url && 'bg-black/80 text-white'}`}>
-        {link.title}
+    <Link
+      href={link.url}
+      className={`rounded px-2 py-1 transition-colors duration-200 ${
+        pathName === link.url
+          ? "bg-black/80 text-white" // Active link styles
+          : "text-black hover:bg-gray-200 hover:text-black" // Default and hover styles
+      }`}
+    >
+      {link.title}
     </Link>
   );
 }
